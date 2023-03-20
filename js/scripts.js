4124b9fde0bf3b88cff9194f1169db9a4d0bc129
Manoj Kumar Sharma <mailme@manojkumarsharma.com>
2023-03-20 22:30:19 +0530

    Added content files

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+﻿/************************
+ *  Toggle the day/night UI experience
+ *****/
+let dayNightElements = [
+    document.querySelector('#dayNight'),
+    document.querySelector('#box1'),
+    document.querySelector('#box2'),
+    document.querySelector('#typedElement')
+];
+dayNight.onclick = function () {
+    dayNightElements.forEach(item => {
+        item.classList.toggle('night');
+    });
+}
+
+/************************
+ *  Animate the typedElement using the external JS Library
+ *****/
+let typingText = new Typed('#typedElement', {
+    strings: ['products', 'customers', 'industries', 'vendors', 'markets', 'competitors'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    cursorChar: '▂',
+    smartBackspace: false,
+    loop: true
+});
